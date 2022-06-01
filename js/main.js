324a6755051188e29113f33d7f145f8beaa736ea
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-06-01 00:34:17 -0700

    theme, header change, project images

--- js/main.js
+++ js/main.js
@@ -1,22 +1,56 @@
 /*----------------------------------
-Ready State Check
+Load Large Images and 
+Handle Loading Screen
 ----------------------------------*/
+
+// const loadingScreen = document.querySelector('.loading-overlay');
 
 const landingText = document.querySelector('.landing-text-container');
 const scrollIndicator = document.querySelector('.scroll-text');
 const scrollArrow = document.querySelector('.arrow');
 
-// will wait for resources to load before enabling animations
-document.addEventListener('readystatechange', () => {
-  if (document.readyState == 'complete') {
-    console.log('animating');
-    landingText.style.display = 'flex';
-    scrollIndicator.style.display = 'block';
-    setTimeout(()=> {
-      scrollArrow.style.display = 'block';
-    }, 4500);
-  }
-})
+// const bgUrl = '/img/border/bg-pattern.png';
+// const background = document.querySelector('.bg-pattern');
+
+// const brdrUrl = '/img/border/brdr.png';
+// const brdr = document.querySelectorAll('.brdr');
+
+// const brdrCorner1 = document.querySelector('.brdr-topleft'); 
+// const brdrCorner2 = document.querySelector('.brdr-topright'); 
+
+// let preloader = document.createElement('img');
+// preloader.src = bgUrl;
+// preloader.addEventListener('load', () => {
+//   background.style.backgroundImage = `url(${bgUrl})`;
+//   preloader = null;
+//   console.log('1');
+//   loadBrdr();
+// });
+
+// function loadBrdr() {
+//   let preloader = document.createElement('img');
+//   preloader.src = brdrUrl;
+//   preloader.addEventListener('load', () => {
+//     brdr.forEach(brdr => {
+//       brdr.style.backgroundImage = `url(${brdrUrl})`;
+//     })
+//     preloader = null;
+//     console.log('2');
+//     go();
+//   });
+// }
+
+go();
+
+function go() {
+  // console.log('hiding overlay');
+  // loadingScreen.style.display = 'none';
+  landingText.style.display = 'flex';
+  scrollIndicator.style.display = 'block';
+  setTimeout(()=> {
+    scrollArrow.style.display = 'block';
+  }, 4500);
+}
 
 /*----------------------------------
 Landing Page
@@ -101,6 +135,7 @@ Page Title Gif Control
 
 // display block from none will start anim from beginning
 const about = document.querySelector('.about-anim');
+const projects = document.querySelector('.projects-anim');
 const contact = document.querySelector('.contact-anim');
 
 window.addEventListener('keyup', (e) => {
@@ -117,41 +152,16 @@ window.addEventListener('keyup', (e) => {
 });
 
 /*----------------------------------
-Hamburger Button 
-----------------------------------*/
-
-const burgerBtn = document.querySelector('.hamburger');
-const navBtns = document.querySelector('.navigation-container');
-
-burgerBtn.addEventListener('click', () => {
-  if (navBtns.classList.contains('show-nav')) {
-    navBtns.classList.remove('show-nav');
-  } else {
-    navBtns.classList.add('show-nav');
-  }
-  
-});
-
-/*----------------------------------
 Navigation Buttons
 ----------------------------------*/
 
-const homeBtn = document.querySelector('.home-btn-container');
 const aboutBtn = document.querySelector('.about-btn-container');
 const workBtn = document.querySelector('.work-btn-container');
 const contactBtn = document.querySelector('.contact-btn-container');
-const stroke1 = document.querySelector('.stroke-1');
 const stroke2 = document.querySelector('.stroke-2');
 const stroke3 = document.querySelector('.stroke-3');
 const stroke4 = document.querySelector('.stroke-4');
 
-homeBtn.addEventListener('click', () => {
-  animReset(stroke4);
-  window.scroll({
-    top: 0,
-    behavior: "smooth"
-  });
-});
 aboutBtn.addEventListener('click', () => {
   animReset(stroke3);
   window.scroll({
@@ -167,7 +177,7 @@ workBtn.addEventListener('click', () => {
   });
 })
 contactBtn.addEventListener('click', () => {
-  animReset(stroke1);
+  animReset(stroke4);
   window.scroll({
     top: screenHeight * 4,
     behavior: "smooth"
@@ -181,3 +191,51 @@ function animReset(stroke) {
     stroke.src = stroke.src.replace(/\?.*$/,"")+"?x="+Math.random();
   }, 750);
 }
+
+/*----------------------------------
+Hamburger Button 
+----------------------------------*/
+
+const burgerBtn = document.querySelector('.hamburger');
+const navBtns = document.querySelector('.navigation-container');
+const nameContainer = document.querySelector('.name-container');
+
+burgerBtn.addEventListener('click', () => {
+  if (navBtns.classList.contains('show-nav')) {
+    navBtns.classList.remove('show-nav');
+    navBtns.classList.add('hide-nav');
+
+    nameContainer.classList.remove('fade-name');
+    nameContainer.classList.add('show-name');
+  } else {
+    navBtns.classList.remove('hide-nav');
+    navBtns.classList.add('show-nav');
+
+    nameContainer.classList.add('fade-name');
+    nameContainer.classList.remove('show-name');
+  }
+});
+
+/*----------------------------------
+Theme Button
+----------------------------------*/
+
+const themeBtn = document.querySelector('.btn-theme');
+const themeOverlay = document.querySelector('.theme-overlay');
+const sunSvg = document.querySelector('.sun-svg');
+const moonSvg = document.querySelector('.moon-svg');
+
+themeBtn.addEventListener('click', () => {
+  if (themeOverlay.classList.contains('show-theme-overlay')) {
+    themeOverlay.classList.remove('show-theme-overlay');
+    themeOverlay.classList.add('hide-theme-overlay');
+    moonSvg.style.display = 'none';
+    sunSvg.style.display = 'block';
+
+  } else {
+    themeOverlay.classList.remove('hide-theme-overlay');
+    themeOverlay.classList.add('show-theme-overlay');
+    sunSvg.style.display = 'none';
+    moonSvg.style.display = 'block';
+  }
+});
